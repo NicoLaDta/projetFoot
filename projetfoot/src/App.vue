cfd58b5f436b9fc406d994753fd3e5a5bd91b163
salavudeenH <salavudeenhadji@gmail.com>
2020-11-17 13:23:36 +0100

    add Tailwind new

---
+++ projetfoot/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+  <v-home></v-home>
+  </div>
+</template>
+
+<script>
+import VHome from '@/components/VHome'
+
+export default {
+  name: 'App',
+  components: {
+   VHome,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
