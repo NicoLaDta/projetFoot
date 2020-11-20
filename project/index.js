8a2abf64ab72f22183b5f6f0551e13f7d5a6e406
NicoLaDta <nicolasboulaud42@gmail.com>
2020-11-20 15:28:41 +0100

    ajout table

--- project/index.js
+++ project/index.js
@@ -81,3 +81,92 @@ app.put('/admin',(req,res) => {
             console.log(err);
     })
 });
+
+//Recupere tout les client
+app.get('/client',(req,res) => {
+    mysqlConnection.query('SELECT * FROM client',(err,rows,fields) => {
+        if(!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Recupere juste un client
+app.get('/client/:id',(req,res) => {
+    mysqlConnection.query('SELECT * FROM admin where IdClient = ?',[req.params.id],(err,rows,fields) => {
+        if(!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Delete un client
+app.delete('/client/:id',(req,res) => {
+    mysqlConnection.query('DELETE FROM client where IdClient = ?',[req.params.id],(err,rows,fields) => {
+        if(!err)
+            res.send('Delete successfully');
+        else
+            console.log(err);
+    })
+});
+
+//Insert un client
+app.post('/client',(req,res) => {
+    let emp = req.body;
+    var sql = "SET @IdClient = ?;SET @NomClient = ?;SET @PrenomClient = ?;SET @Email = ?; SET @TelephoneClient = ?; SET @Password = ?; \
+    CALL AdminAddorEdit(@IdClient,@NomClient,@PrenomClient,@Email,@TelephoneClient,@Password);";
+    mysqlConnection.query(sql,[emp.IdClient, emp.NomClient, emp.PrenomClient, emp.Email, emp.TelephoneClient, emp.Password], (err, rows, fields) => {
+        if(!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                    res.send('Inserted Admin id : '+ element[0].idAdmin);
+            });
+        else
+            console.log(err);
+    })
+});
+
+//Update un client
+app.put('/client',(req,res) => {
+    let emp = req.body;
+    var sql = "SET @IdClient = ?;SET @NomClient = ?;SET @PrenomClient = ?;SET @Email = ?; SET @TelephoneClient = ?; SET @Password = ?; \
+    CALL AdminAddorEdit(@IdClient,@NomClient,@PrenomClient,@Email,@TelephoneClient,@Password);";
+    mysqlConnection.query(sql,[emp.IdClient, emp.NomClient, emp.PrenomClient, emp.Email, emp.TelephoneClient, emp.Password], (err, rows, fields) => {
+        if(!err)
+            res.send('Update successfully')
+        else
+            console.log(err);
+    })
+});
+
+//Recupere toute les reservation
+app.get('/reservation',(req,res) => {
+    mysqlConnection.query('SELECT * FROM reservation',(err,rows,fields) => {
+        if(!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Recupere une reservation
+app.get('/reservation/:id',(req,res) => {
+    mysqlConnection.query('SELECT * FROM reservation where IdReservation = ?',[req.params.id],(err,rows,fields) => {
+        if(!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+//Delete une reservation
+app.delete('/reservation/:id',(req,res) => {
+    mysqlConnection.query('DELETE FROM reservation where IdReservation = ?',[req.params.id],(err,rows,fields) => {
+        if(!err)
+            res.send('Delete successfully');
+        else
+            console.log(err);
+    })
+});
