35f9d812c0771f47ccf67ed2ed304f5829730aa2
victorgbn <49316233+victorgbn@users.noreply.github.com>
2020-11-24 16:57:24 +0100

    Update VLogin

--- vue-foot/src/components/VLogin.vue
+++ vue-foot/src/components/VLogin.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="container-login">
-        <h1>Connexion</h1>
-        <form action="">
-            <label for="">Login</label>
-            <input type="text" placeholder="Login" required>
+  <div class="container-login">
+    <h1>Connexion</h1>
+    <form action="">
+      <label for="">Login</label>
+      <input type="text" placeholder="Login" required />
 
-            <label for="">Password</label>
-            <input type="password" placeholder="Password" required>
+      <label for="">Password</label>
+      <input type="password" placeholder="Password" required />
 
-            <input type="submit" value="Me connecter">
-        </form>
-    </div>
+      <input type="submit" value="Me connecter" />
+    </form>
+  </div>
 </template>
 
 <style lang="postcss" scoped>
 .container-login {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 .container-login h1 {
-    font-size: 1.5rem;
-    font-weight: bold;
-    margin-bottom: 20px;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 20px;
 }
 form {
-    display: flex;
-    margin: auto;
-    flex-direction: column;
+  display: flex;
+  margin: auto;
+  flex-direction: column;
 }
 input {
-    border: solid 1px #000;
-    border-radius: 5px;
-    padding: 1px 5px 1px 5px;
-    margin-bottom: 10px;
+  border: solid 1px #000;
+  border-radius: 5px;
+  padding: 1px 5px 1px 5px;
+  margin-bottom: 10px;
 }
-input[type=submit]{
-background: none;
+input[type="submit"] {
+  background: none;
   border: solid 2px #000;
   padding: 5px;
   border-radius: 5px;
@@ -46,9 +46,22 @@ background: none;
   margin: auto;
 }
 
-input[type=submit]:hover {
+input[type="submit"]:hover {
   background-color: #991917;
   border: solid 2px #991917;
   color: #fff;
 }
 </style>
+
+<script>
+export default {
+  name: "Login",
+  methods: {
+    async getAPIAdmin() {
+      const url = ``;
+      let response = await fetch(url);
+      let json = await response.json();
+    },
+  },
+};
+</script>
