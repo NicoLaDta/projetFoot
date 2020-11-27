afc0af2ac3eb96681fff38ff5bddaab0889e9539
Alan <alanakra@hotmail.fr>
2020-11-27 22:34:01 +0100

    Resp VBoutique

--- vue-foot/src/components/VProduct.vue
+++ vue-foot/src/components/VProduct.vue
@@ -1,11 +1,11 @@
 <template>
  <div class="container-product">
-  <div class="pres flex">
+  <div class="pres flex flex-col md:flex-row">
 
-   <div class="container-image">
+   <div class="container-image md:w-6/12 w-full">
      <img src="@/assets/image-boutique/maillot_1_product.jpg" alt="" class="responsive">
    </div>
-    <div class="container-text">
+    <div class="container-text md:w-6/12 w-full">
      <h1 class="font-semibold">Title</h1>
      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto enim distinctio placeat accusamus adipisci id magnam harum tempore quaerat unde consequuntur quisquam iure similique, temporibus nobis in sed. Quaerat soluta tempore fuga esse similique nobis, odio reiciendis accusamus earum, atque voluptas. Nostrum voluptates, eaque inventore eius odit animi suscipit voluptatibus laudantium quaerat sint possimus, porro quasi. Voluptatem eum at repudiandae rerum. At neque dignissimos, ullam reiciendis laboriosam rerum alias voluptate amet qui eius fugiat voluptas blanditiis repellat eveniet earum officia?</p>
     </div>
@@ -25,7 +25,22 @@
   }
 
   .container-image, .container-text{
-    width: 50%
+    /* width: 50% */
+  }
+
+   @media (max-width: 400px) {
+    .container-product{
+     margin-left: 70px;
+     margin-right: 70px;
+    }
+  }
+
+
+  @media (max-width: 400px) {
+    .container-product{
+     margin-left: 40px;
+     margin-right: 40px;
+    }
   }
 </style>
 
