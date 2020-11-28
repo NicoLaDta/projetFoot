396e591fc23f9bdb82d50e8047b5bef498740722
JustineLou <71087008+JustineLou@users.noreply.github.com>
2020-11-28 10:47:44 +0100

    add billetterie page

--- vue-foot/src/components/VBilletterie.vue
+++ vue-foot/src/components/VBilletterie.vue
@@ -1,86 +1,211 @@
 <template>
-  <div class="container">
-    <h1 class="title font-bold not-italic text-center">Prochains match</h1>
-     
-     <div class="match-item flex justify-center items-center flex-col">
-       <p class="conditions font-bold text-center">Date/heure/lieu prochain match</p>
+  <div class="container-billetterie">
+    <div class="billetterie">
+      <div class="billetterie-bloc">
+        <div class="time-location">
+          <p>12/01/2020 - 21:00</p>
+          <p>Stade1</p>
+        </div>
+        <div class="team-scores">
+          <div class="team">
+            <img :src="logoREDSTAR" alt="logo-redstar" /> <br />
+            <p>Redstar Football Club</p>
+          </div>
+          <div class="VS">
+            <p>VS</p>
+          </div>
+          <div class="team">
+            <img :src="logoPARISFC" alt="logo-tfc" /> <br />
+            <p>Paris Football Club</p>
+          </div>
+        </div>
+        <div class="button">
+          <button type="button">Billets</button>
+        </div>
+      </div>
 
-         <div class="versus flex justify-around font-bold items-center uppercase">
-           <div class="first-team">
-             <div class="team-img bg-gray-200"></div>
-             <p>Nom Equipe 1</p>
-           </div>
-             <p>VS</p>
-           <div class="second-team">
-             <div class="team-img bg-gray-200"></div>
-             <p>Nom Equipe 2</p>
-           </div>
-         </div>
-
-         <div class="buy-tickets text-center px-3 rounded text-white">
-           Billets
-         </div>
-     </div>
-
-    <p class="title font-bold not-italic text-center">Ou nous trouver ?</p>
-
-    <div class="find-us flex justify-center items-center">
-      <div class="map bg-green-300 justify-center"></div>
-
-      <div class="adress">
-        <p>RedStar Football Club</p>
-        <p>92 Rue du Dr Bauer</p>
-        <p>93400 Saint-Ouen</p>
+      <div class="billetterie-bloc">
+        <div class="time-location">
+          <p>12/01/2020 - 21:00</p>
+          <p>Stade2</p>
+        </div>
+        <div class="team-scores">
+          <div class="team">
+            <img :src="logoREDSTAR" alt="logo-redstar" /> <br />
+            <p>Redstar Football Club</p>
+          </div>
+          <div class="VS">
+            <p>VS</p>
+          </div>
+          <div class="team">
+            <img :src="logoPARISFC" alt="logo-tfc" /> <br />
+            <p>Paris Football Club</p>
+          </div>
+        </div>
+        <div class="button">
+          <button type="button">Billets</button>
+        </div>
+      </div>
+      <div class="billetterie-bloc">
+        <div class="time-location">
+          <p>12/01/2020 - 21:00</p>
+          <p>Stade3</p>
+        </div>
+        <div class="team-scores">
+          <div class="team">
+            <img :src="logoREDSTAR" alt="logo-redstar" /> <br />
+            <p>Redstar Football Club</p>
+          </div>
+          <div class="VS">
+            <p>VS</p>
+          </div>
+          <div class="team">
+            <img :src="logoPARISFC" alt="logo-tfc" /> <br />
+            <p>Paris Football Club</p>
+          </div>
+        </div>
+        <div class="button">
+          <button type="button">Billets</button>
+        </div>
+      </div>
+      <div class="billetterie-bloc">
+        <div class="time-location">
+          <p>12/01/2020 - 21:00</p>
+          <p>Stade4</p>
+        </div>
+        <div class="team-scores">
+          <div class="team">
+            <img :src="logoREDSTAR" alt="logo-redstar" /> <br />
+            <p>Redstar Football Club</p>
+          </div>
+          <div class="VS">
+            <p>VS</p>
+          </div>
+          <div class="team">
+            <img :src="logoPARISFC" alt="logo-tfc" /> <br />
+            <p>Paris Football Club</p>
+          </div>
+        </div>
+        <div class="button">
+          <button type="button">Billets</button>
+        </div>
       </div>
     </div>
 
+    <div class="find-us-container">
+      <h1>Où nous trouver ?</h1>
 
+      <div class="find-us">
+        <div class="map bg-green-300 justify-center"></div>
+
+        <div class="adress">
+          <p>RedStar Football Club</p>
+          <p>92 Rue du Dr Bauer</p>
+          <p>93400 Saint-Ouen</p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <style lang="postcss" scoped>
- .container{
-   font-family: Roboto Condensed;
- }
+.container-billetterie {
+  font-family: Roboto Condensed;
+  margin-right: 100px;
+  margin-left: 100px;
+  display: flex;
+  margin-bottom: 40px;
+  flex-direction: column;
+}
 
- .match-item{
-   background: #FFFFFF;
-   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-   border-radius: 10px;
-   width: 627px;
-   height: 337px;
- }
+.billetterie {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: auto;
+  max-width: 1300px;
+}
 
- .conditions {
-   font-style: normal;
-   font-size: 24px;
-   line-height: 28px;
- }
+.billetterie-bloc {
+  border-radius: 10px;
+  padding: 50px;
+  width: 550px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  border: 2px solid #000000;
+  height: 320px;
+  font-size: 1.2rem;
+  margin: 10px;
+}
 
- .versus{
-   font-style: normal;
- }
+.versus {
+  font-style: normal;
+}
 
- .team-img{
-   width: 100px;
-   height: 100px;
- }
+.team-img {
+  width: 100px;
+  height: 100px;
+}
 
- .buy-tickets{
-   background: rgba(12, 68, 55, 0.9);
-   box-sizing: border-box;
-   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-   display: inline;
- }
+.find-us-container {
+  display: flex;
+  flex-direction: column;
+}
 
- .map{
+.find-us-container h1 {
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-align: center;
+}
+
+.find-us {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+
+.map {
   width: 574px;
   height: 321px;
- }
+}
+
+.adress {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-size: 1.3rem;
+}
+
+@media screen and (max-width: 800px) {
+  .container-billetterie {
+    margin: auto;
+  }
+  
+  .billetterie-bloc {
+    width: 300px;
+    font-size: 1rem;
+  }
+
+  .find-us-container {
+    margin: auto;
+  }
+
+  .find-us {
+    flex-direction: column-reverse;
+    margin: auto;
+    text-align: center;
+    font-size: 1rem;
+  }
+
+  .map {
+    width: 90%;
+  }
+}
 </style>
 
 <script>
-export default {
- 
-}
+export default {};
 </script>
+
